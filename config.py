afd3930d1ebc8e999b1c15d80e616b12cbb3dafe
Bruno Rocha <rochacbruno@gmail.com>
2021-03-09 18:15:25 -0300

    8th

--- config.py
+++ config.py
@@ -3,24 +3,25 @@ from dynaconf import Dynaconf, Validator
 
 # -- Then you create your `settings` instance
 settings = Dynaconf(
-    settings_files=[                  # Paths or globs to any toml|yaml|ini|json|py
-        "default_settings.toml",      # a file for default settings
-        "settings.toml",              # a file for main settings
-        ".secrets.toml"               # a file for sensitive data (gitignored)
+    settings_files=[                      # Paths or globs to any toml|yaml|ini|json|py
+        "configs/default_settings.toml",  # a file for default settings
+        "configs/settings.toml",          # a file for main settings
+        "configs/.secrets.toml"           # a file for sensitive data (gitignored)
     ],
 
-    environments=True,                # Enable layered environments
-                                      # (sections on config file for development, production, testing)
+    environments=True,                    # Enable layered environments
+                                          # (sections on config file for development, production, testing)
 
-    load_dotenv=True,                 # Load envvars from a file named `.env`
-                                      # TIP: probably you don't want to load dotenv on production environments
-                                      #      pass `load_dotenv={"when": {"env": {"is_in": ["development"]}}}
+    load_dotenv=True,                     # Load envvars from a file named `.env`
+                                          # TIP: probably you don't want to load dotenv on production environments
+                                          #      pass `load_dotenv={"when": {"env": {"is_in": ["development"]}}}
 
-    envvar_prefix="DYNACONF",         # variables exported as `DYNACONF_FOO=bar` becomes `settings.FOO == "bar"`
-    env_switcher="ENV_FOR_DYNACONF",  # to switch environments `export ENV_FOR_DYNACONF=production`
+    envvar_prefix="DYNACONF",             # variables exported as `DYNACONF_FOO=bar` becomes `settings.FOO == "bar"`
+    env_switcher="ENV_FOR_DYNACONF",      # to switch environments `export ENV_FOR_DYNACONF=production`
 
-    dotenv_path="envfile/.env"        # custom path for .env file to be loaded
+    dotenv_path="configs/.env"            # custom path for .env file to be loaded
 )
+
 # More options on https://www.dynaconf.com/configuration/
 
 
@@ -60,4 +61,4 @@ settings.validators.validate()
 # NOTE: On Dynaconf 4.0.0 all the above will be also possible as a pydantic schema :)
 
 
-# NOW go and read the `default_settings.toml` file
+# NOW go and read the `configs/default_settings.toml` file
